b309b2f72ed20954ccad0198a5e341bed57425ff
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-04-18 22:20:51 -0700

    Login Form Functionality
    Created the functionality for the login form. The user is able to sign up, sign in, and sign out. The app will also through error messages if  any invalid emails/passwords are entered.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import { signup } from './firebaseConfig';
-import { useRef } from 'react';
+import { signup, logout, login, useAuth } from './firebaseConfig';
+import { useRef, useState } from 'react';
 import './index.css';
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const currentUser = useAuth()
+
   const emailRef = useRef()
   const passwordRef = useRef()
 
-  // call to firebase api
+  // Handle signup api call to firebase
   async function handleSignup() {
-    await signup(email, password)
+    setLoading(true)
+    try {
+      // Grab current value of the email/password ref
+      await signup(emailRef.current.value, passwordRef.current.value)
+    } catch (error) {
+      alert('error')
+    }    
+    setLoading(false)
+  }
+
+  // Handle login 
+  async function handleLogin() {
+    setLoading(true)
+    try {
+      await login(emailRef.current.value, passwordRef.current.value)
+    } catch (error) {
+      alert('error')
+    }    
+    setLoading(false)
+  }
+
+  // Handle Logout api call to firebase
+  async function handleLogut() {
+    setLoading(true)
+    try {
+      await logout()
+    } catch (error) {
+      alert('Error')
+    }
+    setLoading(false)
   }
 
   return (
     <div className="App">
+
+      <div>Currently Logged In As: { currentUser?.email } </div>
 
       <div className='fields'>
         <input ref={emailRef} placeholder='email' />
         <input ref={passwordRef} type='password' placeholder='Password' />
       </div>
 
-      <button onClick={handleSignup}>Sign Up</button>
+      {/* Disables singup button when user is signed in */}
+      <button disabled={loading || currentUser} onClick={handleSignup}>Sign Up</button>
+      <button disabled={loading || currentUser} onClick={handleLogin}>Log In</button>
+      <button disabled={loading || !currentUser} onClick={handleLogut} >Log Out</button>
     </div>
   );
 }
