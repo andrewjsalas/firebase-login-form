16cd0279d440493335195b668ef9a86294f91869
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-04-19 22:17:01 -0700

    Create Routes
    Created routes from React-Router and created all necessary pages and components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import { BrowerRouter as Router, Routes, Route } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/reactToastify.css'
-
+import PrivateRoute from './Components/PrivateRoute';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Home from './pages/Home';
 
 function App() {
-
-
   return (
     <>
       <Router>
         <Routes>
-          
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<PrivateRoute/>}> 
+            <Route path='/profile' element={<Profile/>} />
+          </Route>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
       </Router>
+
+      {/* Toastify for displaying error messages */}
+      <ToastContainer />
     </>
   );
 }
-
 
 export default App;
